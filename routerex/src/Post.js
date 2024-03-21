a57e079cfe76540236d5ad8b93f65e84f78b3183
Mingguriguri <rlaalswjd-@naver.com>
2024-03-21 16:08:31 +0900

    Feat(Post): useParams()를 이용하여, path 후킹

---
+++ routerex/src/Post.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import BlogPosts from "./BlogPosts";
+
+export function Post() {
+    const {slug} = useParams();
+    const post = BlogPosts[slug];
+
+    if(!post) {
+        return <span> The blog post you've requested doesn't exit.</span>;
+    }
+
+    const {title, description} = post;
+    
+    return (
+        <div style={{ padding: 20 }}>
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </div>
+    );
+}
